d04d793a34071e2208916313c7c57bf4e867bdd6
Tanapan Panpak <64010323@kmitl.ac.th>
2021-08-28 13:07:16 +0700

    Add project files.

---
+++ Week3.1/WEEK3.1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+	float popA, popB ;
+	float grA, grB;
+	int year = 0;
+	printf("Population of A = ");
+	scanf_s("%f", &popA);
+	printf("Growth rate (%%) = ");
+	scanf_s("%f", &grA);
+	printf("Population of B = ");
+	scanf_s("%f", &popB);
+	printf("Growth rate (%%) = ");
+	scanf_s("%f", &grB);
+	printf("year %d, A = %.2f ,B = %.2f\n", year, popA, popB);
+	do {
+		year = year + 1;
+		popA = popA * ((100 + grA) / 100);
+		popB = popB * ((100 + grB) / 100);
+		printf("year %d, A = %.2f ,B = %.2f\n", year, popA, popB);
+	} while (popA <= popB);
+	printf("Duration =   %d years", year);
+}
